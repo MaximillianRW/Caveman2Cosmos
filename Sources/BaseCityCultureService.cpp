9147955142257a6575134d722b1c8e26d447901b
flabbert <flabbert@iddqd.no>
2022-11-06 13:56:11 +0100

    little bit more tinkering with basics

--- Sources/BaseCityCultureService.cpp
+++ Sources/BaseCityCultureService.cpp
@@ -1,1 +1,13 @@
 #include "BaseCityCultureService.h"
+
+
+
+uint64_t BaseCityCultureService::GetCulture(PlayerTypes player) const
+{
+	uint64_t returnValue = 0;
+
+	returnValue = returnValue + _cultureBlocks[player] * _blockSize;
+	returnValue = returnValue + _cultureAmounts[player];
+
+	return returnValue;
+}
--- Sources/BaseCityCultureService.h
+++ Sources/BaseCityCultureService.h
@@ -9,23 +9,36 @@
 class BaseCityCultureService
 {
 protected:
+	static const int _blockSize = 100000;
 	CvCity* city;
-	int _cultureLevel;
-	int _cultureAmount;
 	
+	int _cultureLevel;
+	int* _cultureAmounts;
+	int* _cultureBlocks;
 
 public:
 	BaseCityCultureService(CvCity* c) {
 		city = c;
+		_cultureAmounts = new int[MAX_PLAYERS];
+		_cultureBlocks = new int[MAX_PLAYERS];
+		_cultureLevel = 0;
 	}
+	~BaseCityCultureService() {
+		SAFE_DELETE_ARRAY(_cultureAmounts);
+		SAFE_DELETE_ARRAY(_cultureBlocks);
+	}
+
+
 	virtual CultureLevelTypes GetCultureLevel() const;
+	virtual PlayerTypes FindMostCulturedPlayer() const;
 	virtual int GetCultureThreshhold() const;
 	virtual int CalculateCityOutput();
 	virtual int AddCulture(int amount, PlayerTypes ePlayer);
 	virtual int RemoveCulture(int amount, PlayerTypes ePlayer);
 	virtual bool RemovePlayerCulture(PlayerTypes ePlayer);
-	virtual int GetCulture(PlayerTypes ePlayer);
+	virtual uint64_t GetCulture(PlayerTypes ePlayer) const;
 	virtual void DoPlotCulture();
+
 
 };
 
